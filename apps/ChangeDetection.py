bfc6de57c1a46bfceca6b3d06da8ade818d18e1b
hardicd <113914097+hardicd@users.noreply.github.com>
2022-12-06 13:44:37 -0600

    Pushing Change Detection app

--- apps/ChangeDetection.py
+++ apps/ChangeDetection.py
@@ -8,12 +8,22 @@ st.set_page_config(layout="wide")
 
 def app():
     st.title("Change Detection")
-
-    @st.cache(persist=True)
-    def ee_authenticate(token_name="EARTHENGINE_TOKEN"):
-        geemap.ee_initialize(token_name=token_name)
+    json_private_key = {
+  "type": "service_account",
+  "project_id": "change-370819",
+  "private_key_id": "cdd38e86bb6112d0503fe20d7d69852e5a5e6be3",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDhO1LO8BEsdwxX\nEJC0UmRHW3EUKwzJRSjUwNr6QOr8B0LG25jY2Qpw8WJK19+wxCy/sIWqWJa1AkqF\nVniRqNMnTBpznHnQ0bBzDgJkDqrRkhsBs+mTjy/ikp5scHP6N4rqKUCngvfcxzIs\nNs9TYjqS1pM8Qjmt025g7zy000YcglyscXcdK8/jO6L6AUjZGYe48vGOM18+/6XD\nSqTw3Orb0jYSaHfoeNayGPdJgUDK8Y4Kc/hgYrCDxTNKogFfg3qTT4TZW7QcoxrK\nk6G5HIbdbQNus5fRqxiZmzlfDvkohLso+jtQIinQtiYqz1bMkydr9bcNKoPfn6aW\n+ibHqKXhAgMBAAECggEABdGlCuChPkoDFnAV9Pqt9DlUu2XTVOiFrLLLgCyo2thg\ngse4nrLuIKCJNBAjYNXoprkrnYzgCTl6bbsgA/oIc9r0/KiobbCFLlZzJweepSnG\nVdhtP+6H7lgbDLJ+ZJKxHZScQtemNLIsGyqoAj+m7hU/b46cc9+4WdEZ0JLRcboQ\nvLW/Ga0Ajv0ZX6lMIFvvMcDSXCI7FFPYHYVabI4ZzxBv+Jyt6/BenAWv0nkvDJuu\n8hY6jSW5wU9uJLalyoUvSkpsXfJv6bDnoMh2Fbqj8ZEech7ijGBIcpuG6kSMniAE\nMqBEzFSTOLodxXseyzuWHrsaE/0LMfZHBOYxrwKJcwKBgQDypKcwMB48TIutKJuE\n3k5fET0+faR4avFcgyZOjOyUZ3+l1ORNp0J+lPw1lmHvxsKJzbEm6EH/yjmzXLBS\nl5Tif+D2+ybH4REQ01yW7xGack1yODyCtt/pS5aXvo9Cw+WeoXAVDI/8BA74GusU\n55M7Sv4vyJggV48x27GNfykdkwKBgQDtoU6TuFiCVBY9a5dwuP0r4KHziAO3/8nH\nHczNpcF2W2NqnDfBbs3iufVkw/rTDRggnw/7nwtCrIOYGV5xPnkrKwla4lgFfP2E\nJ27aeoE1UGCa0++pL9Xe6QXjTV8lpZ2TbG4jp0daryTZZx91oDLMAIbqkJRtaKUV\noLdps4L3OwKBgQCBuG89fuwbNp0R9Py32xWxE4lXiFpOmnXxDJiFNCgi6vY+VQO9\nVCdjfwkbKQuw4eUNQS9taowmURnp/yqw7SMGuHEv+XNxNq0l/qmoc6VImcU3xELt\naxFoKyYWeCUk/5AZ75r5Vd8AT7clA1cctPVspJoiN67E06rVb+uLhykXCwKBgQCR\nKo26KE/Jrly3wNaPAjOHbn8BKwI6kYROo6HNr4j0KPge1Lgq7unPrBKBMEg2TjWD\nGCjHam2SmZj6feT4AVY8TNo2LzfegnDNHtnVlikAzM13SCmMjbB1sJzsMwQBF5+f\nxAm0tigc+gnk1d6eGPNvNJM3EIPcIqVXBQuHAybqqQKBgQDaXnEgdhb/iz6gwHTV\njc6FKtR+A+i5G9JKfc4JRHPpkYsbKNYv+W0Gp6oeLIumTqTkIXK1Uhjv5+qWoE1i\nLW4hhzLwBV6HvrXD+ef5exehK88Hl+pkvVpOm5TEqmPtaAsXqtfQ8yfFD2YvmJg2\no2m6keG1mXre7Wd189iXr4FIBA==\n-----END PRIVATE KEY-----\n",
+  "client_email": "eogmap@change-370819.iam.gserviceaccount.com",
+  "client_id": "116630732010339894634",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/eogmap%40change-370819.iam.gserviceaccount.com"
+}
+    service_account = 'eogmap@change-370819.iam.gserviceaccount.com'
+    credentials = ee.ServiceAccountCredentials(service_account, json_private_key)
+    ee.Initialize(credentials)
     col1, col2 = st.columns([4, 1])
-    ee_authenticate(token_name='4/1AfgeXvvqlc4MSQAZZ60t4KKUk_aNPzG15UT1GOju2sAeN18UXUGMTOkecP0')
     Map = geemap.Map()
     Map.add_basemap("HYBRID")
 
